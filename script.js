b43dde3957e19af9d71626ce2a3b032f24b9ea9e
Amany Mohamed <109109277+Amanymohamed98@users.noreply.github.com>
2025-03-06 04:54:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    const buyBtns = document.querySelectorAll(".buy-btn");
+    buyBtns.forEach(btn => {
+        btn.addEventListener("click", function () {
+            alert("Product added to cart!");
+        });
+    });
+});
